d25235ee636fed09c4ef38069ab30a034d292200
Raj Manian <sraja2911@gmail.com>
2018-09-19 13:03:13 -0400

    Tileextraction
    Tile Extraction bash script

--- code/tile_extraction.sh
+++ code/tile_extraction.sh
@@ -1,8 +1,8 @@
 nvidia-docker run \
-     -it --rm -p6666:8888 \
+     -it --rm -p7777:8888 \
      -v "${PWD}:/app:rw" \
-     -v "/home/raj/github/histo_tile_classifier:/data/code:rw" \
-     -v "/home/raj/github/histo_tile_classifier/results:/data/output/results:rw" \
+     -v "/home/raj/github/tcga_multiclass_classifier_CNN/code:/data/code:rw" \
+     -v "/home/raj/github/tcga_multiclass_classifier_CNN/results:/data/output/results:rw" \
      -v "/media/raj/Raj1_5/100k_tiles_data/train:/data/train:rw" \
      -v "/media/raj/Raj1_5/100k_tiles_data/test:/data/test:rw" \
      fgiuste/histomicstk
